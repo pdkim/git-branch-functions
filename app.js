77b96d549931d965d38680d029f2498560d88242
Phillip Kim <phildkim@gmail.com>
2018-01-20 16:12:27 -0800

    got question 4 working.  ready for pull request

--- app.js
+++ app.js
@@ -82,16 +82,17 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(testArray) {
   //eslint-disable-line
-  var addAll = 0;
-  for (i = 0; i < testArray.length; i++) {
-    addAll = sum(testArray.length[], )
+  var add2 = 0;
+  for (var i = 0; i<testArray.length; i++) {
+    add2 = sum(testArray[i], add2)[0];
   }
-  return []
+  var addMessage = testArray + ' was passed in as an array of numbers, and ' + add2 + ' is their sum.';
+  return [add2, addMessage];
 }
-
+sumArray(testArray);
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
